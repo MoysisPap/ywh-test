7153617bda9ff6a95479e31b4a27197525e94e85
Carmela Pippa <carmelapippa@gmail.com>
2024-08-15 12:25:21 +0200

    test service-worker

--- service-worker.js
+++ service-worker.js
@@ -38,3 +38,19 @@ self.addEventListener("activate", (event) => {
     })
   );
 });
+
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("/service-worker.js").then(
+      (registration) => {
+        console.log(
+          "ServiceWorker registration successful with scope: ",
+          registration.scope
+        );
+      },
+      (err) => {
+        console.log("ServiceWorker registration failed: ", err);
+      }
+    );
+  });
+}
